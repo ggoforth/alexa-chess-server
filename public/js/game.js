a4fa9b5e1fcacc0111b4601a8e73e5d623bed4db
Greg Goforth <greg.goforth@gmail.com>
2016-08-14 21:46:38 -0700

    refactor

--- public/js/game.js
+++ public/js/game.js
@@ -1,33 +1,42 @@
 (function () {
+  /**
+   * The board object, will be set near the end of the script.
+   */
   var board,
-    game = new Chess(),
-    statusEl = $('#status'),
-    fenEl = $('#fen'),
-    pgnEl = $('#pgn');
 
+    /**
+     * The game validation engine.
+     */
+    game = new Chess(),
+
+    /**
+     * The status of the game, IE whos turn it is.
+     * 
+     * @type {*|jQuery|HTMLElement}
+     */
+    statusEl = $('#status'),
+
+    /**
+     * The fen element.
+     * 
+     * @type {*|jQuery|HTMLElement}
+     */
+    fenEl = $('#fen');
+
+  /**
+   * The status updating function.
+   */
   var updateStatus = function () {
     var status = '';
 
     var moveColor = 'White';
-    if (game.turn() === 'b') {
-      moveColor = 'Black';
-    }
-
-    // checkmate?
-    if (game.in_checkmate() === true) {
-      status = 'Game over, ' + moveColor + ' is in checkmate.';
-    }
-
-    // draw?
-    else if (game.in_draw() === true) {
-      status = 'Game over, drawn position';
-    }
-
-    // game still on
+    
+    if (game.turn() === 'b') moveColor = 'Black';
+    if (game.in_checkmate() === true) status = 'Game over, ' + moveColor + ' is in checkmate.';
+    else if (game.in_draw() === true) status = 'Game over, drawn position';
     else {
       status = moveColor + ' to move';
 
-      // check?
       if (game.in_check() === true) {
         status += ', ' + moveColor + ' is in check';
       }
@@ -35,7 +44,6 @@
 
     statusEl.html(status);
     fenEl.html(game.fen());
-    pgnEl.html(game.pgn());
   };
 
   var cfg = {
@@ -51,8 +59,6 @@
   var socket = io.connect('http://chess.shift3sandbox.com:8080');
 
   socket.on('move piece', function (data) {
-    console.log('Moving piece: ', data);
-
     var move = game.move({
       from: data.from,
       to: data.to,
@@ -63,6 +69,7 @@
     if (!move) return;
 
     board.move(data.from + '-' + data.to);
+
     updateStatus();
   });
 }());
